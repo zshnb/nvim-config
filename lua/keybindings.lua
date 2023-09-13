347ab6cada7d995c0bca0c8897c1858cca2f1a99
zshnb <a857681664@gmail.com>
2023-09-13 23:39:28 +0800

    remove unname paste

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.api.nvim_set_option("clipboard","unnamed")
 local map = vim.api.nvim_set_keymap
 -- 复用 opt 参数
 local opt = {noremap = true, silent = true }
